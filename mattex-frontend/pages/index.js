030ccab7c6374981676c7de9380e0cca99b59d98
Владислав Лебедев <vlad-057@mail.ru>
2023-04-13 20:32:22 +0600

    init

--- mattex-frontend/pages/index.js
+++ mattex-frontend/pages/index.js
@@ -43,7 +43,7 @@ export default function Home() {
     return (
         <div>
             <Head>
-                <title>Mattex</title>
+                
                 <link rel="icon" href="/" />
             </Head>
 
