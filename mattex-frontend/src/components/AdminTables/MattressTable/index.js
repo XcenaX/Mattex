85a92028a9bfe6fc69152e0cd2cdfc148f821ab3
Владислав Лебедев <vlad-057@mail.ru>
2023-04-13 14:06:47 +0600

    init

--- mattex-frontend/src/components/AdminTables/MattressTable/index.js
+++ mattex-frontend/src/components/AdminTables/MattressTable/index.js
@@ -4,44 +4,12 @@ import {useState} from "react";
 import axios from "axios";
 import helpers from "../../../helpers";
 
-export default function MattressCasesTable(props){
-    const [newCaseTitle, setNewCaseTitle] = useState('');
-    const [newCaseDescription, setNewCaseDescription] = useState('');
-    const [newCasePrice, setNewCasePrice] = useState(0);
-    const [newCaseImgSrc, setNewCaseImgSrc] = useState('');
-
-    const createCase = () => {
-        let data = {
-            title: newCaseTitle,
-            description: newCaseDescription,
-            price: +newCasePrice,
-            imgSrc: newCaseImgSrc
-        };
-
-        axios.post(helpers.serverDomain + '/mattresses/cases', data)
-            .then(function (response) {
-                alert('Чехол добавлен успешно!');
-            })
-            .catch(function (error) {
-                alert('Что-то пошло нетак!');
-            });
-    }
-
-    const deleteCase = (id) => {
-        axios.delete(helpers.serverDomain + '/mattresses/cases/' + id)
-            .then(function (response) {
-                alert('Чехол удален успешно!');
-            })
-            .catch(function (error) {
-                alert('Что-то пошло нетак!');
-            });
-    }
-
+export default function MattressTable(props){    
     return(
         <>
             <div className={styles.admin_block}>
                 <div className={styles.widgets}>
-                    <h2>Чехлы</h2>
+                    <h2>Матрасы</h2>
                     <div className={styles.add_button} title="Добавить"
                         onClick={() => {
                             // props.setModalBody(<AddLayerBody/>);                            
@@ -54,24 +22,29 @@ export default function MattressCasesTable(props){
                     <table className={styles.fl_table}>
                         <thead>
                             <tr>
-                            <th>Наименование</th>
-                            <th>Описание</th>
-                            <th>Цена</th>
+                            <th>Название</th>
+                            <th>Ткань</th>
+                            <th>Высота(см)</th>
+                            <th>Жесткость</th>
+                            <th>Цена(KZT)</th>                            
                             <th>Изображение</th>
+                            <th></th>
                             <th></th>    
                             </tr>
                         </thead>
                         <tbody>
-                            {props.cases.length === 0 ? (
+                            {props.matresses.length === 0 ? (
                                 <tr>
                                     <td colSpan={5}>Нет данных</td>
                                 </tr>
                                 ) : (                            
-                                    props.cases.map(item => (
+                                    props.matresses.map(item => (
                                         <tr key={item.id} className={styles.table_row}>
-                                            <td>{item.title}</td>
-                                            <td>{item.description}</td>
-                                            <td>{item.price}</td>
+                                            <td>{item.name}</td>
+                                            <td>{item.cloth}</td>
+                                            <td>{item.height}</td>
+                                            <td>{item.load}</td>
+                                            <td>{item.price}</td>                                            
                                             <td><a href={item.imgSrc} target="_blank" width={'100%'}>Посмотреть</a></td>
                                             <td className={styles.table_cell}><div onClick={() => deleteCase(item._id)} className={styles.delete_btn2}></div></td>
                                         </tr>
